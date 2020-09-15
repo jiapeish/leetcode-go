e2323cdaaf27a191bb50df7649660fed79abb6f0
jiapeish <jiapeish@gmail.com>
2020-09-16 00:00:01 +0800

    299

--- leetcode/0299-bulls-and-cows/bulls-and-cows.go
+++ leetcode/0299-bulls-and-cows/bulls-and-cows.go
@@ -1,5 +1,29 @@
 package _299_bulls_and_cows
 
-func getHint(secret string, guess string) string {
+import "strconv"
 
+func getHint(secret string, guess string) string {
+	bulls, cows := 0, 0
+	numbers := make([]int, 10)
+	length := len(secret)
+
+	for i := 0; i < length; i++ {
+		s := secret[i]
+		g := guess[i]
+		if s == g {
+			bulls++
+		} else {
+			if numbers[s-'0'] < 0 {
+				cows++
+			}
+
+			if numbers[g-'0'] > 0 {
+				cows++
+			}
+
+			numbers[s-'0']++
+			numbers[g-'0']--
+		}
+	}
+	return strconv.Itoa(bulls) + "A" + strconv.Itoa(cows) + "B"
 }
