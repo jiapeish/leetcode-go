483eef2168cf5a3d348b295c2711946e6e4b8c2c
jiapeish <jiapeish@gmail.com>
2020-09-18 00:08:49 +0800

    problem 389

--- leetcode/0389-find-the-difference/find-the-difference.go
+++ leetcode/0389-find-the-difference/find-the-difference.go
@@ -1,1 +1,21 @@
 package _389_find_the_difference
+
+func findTheDifference(s string, t string) byte {
+
+	rec := make([]int, 26)
+
+	for i := range s {
+		rec[s[i] - 'a']++
+		rec[t[i] - 'a']--
+	}
+	rec[t[len(t)-1] - 'a']--
+
+	var idx, count int
+
+	for idx, count = range rec {
+		if count == -1 {
+			break
+		}
+	}
+	return byte('a' + idx)
+}
