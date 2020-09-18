0341406ebaf534c598dfe2490ed2f5472d113adf
jiapeish <jiapeish@gmail.com>
2020-09-18 23:17:29 +0800

    0409

--- leetcode/0409-longest-palindrome/longest-palindrome.go
+++ leetcode/0409-longest-palindrome/longest-palindrome.go
@@ -1,1 +1,24 @@
 package _409_longest_palindrome
+
+func longestPalindrome(s string) int {
+	var record [256]int
+	for i := range s {
+		record[s[i]]++
+	}
+
+	var length int
+	var hasOdd int
+	for _, count := range record {
+		if count == 0 {
+			continue
+		}
+
+		if count & 1 == 0 {
+			length += count
+		} else {
+			hasOdd = 1
+			length += count - 1
+		}
+	}
+	return length + hasOdd
+}
--- leetcode/0447-number-of-boomerangs/number-of-boomerangs.go
+++ leetcode/0447-number-of-boomerangs/number-of-boomerangs.go
@@ -1,1 +1,5 @@
 package _447_number_of_boomerangs
+
+func numberOfBoomerangs(points [][]int) int {
+
+}
