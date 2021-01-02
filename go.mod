36cdd1f39b41fe179d4ef0b6840f97e55097c721
jiapeish <jiapeish@gmail.com>
2021-01-02 23:57:14 +0800

    add frame dir

---
+++ go.mod
@@ -0,0 +1,5 @@
+module leetcode-go
+
+go 1.13
+
+require gopkg.in/macaron.v1 v1.4.0
--- go/library/container/ring/main.go
+++ go/library/container/ring/main.go
@@ -4,3 +4,19 @@ import (
 	"container/ring"
 	"fmt"
 )
+
+func main() {
+	ring := ring.New(3)
+
+	for i := 1; i <= 3; i++ {
+		ring.Value = i
+		ring = ring.Next()
+	}
+
+	// 计算 1+2+3
+	s := 0
+	ring.Do(func(p interface{}){
+		s += p.(int)
+	})
+	fmt.Println("sum is", s)
+}
