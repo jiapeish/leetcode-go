d54e200680227bb200c106a269b254d7be58c4af
jiapeish <jiapeish@gmail.com>
2021-02-17 22:40:11 +0800

    BFS: right to left, 0513

---
+++ leetcode/0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.go
@@ -0,0 +1,25 @@
+package main
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func findBottomLeftValue(root *TreeNode) int {
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		root = queue[0]
+		queue = queue[1:]
+
+		if root.Right != nil {
+			queue = append(queue, root.Right)
+		}
+
+		if root.Left != nil {
+			queue = append(queue, root.Left)
+		}
+	}
+	return root.Val
+}
