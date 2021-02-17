3f27856b81b238d8745f6d87e0dee2337b29090f
jiapeish <jiapeish@gmail.com>
2021-02-17 22:55:18 +0800

    dfs 0513

--- leetcode/0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.go
+++ leetcode/0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.go
@@ -6,7 +6,7 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func findBottomLeftValue(root *TreeNode) int {
+func findBottomLeftValue1(root *TreeNode) int {
 	queue := []*TreeNode{root}
 
 	for len(queue) > 0 {
@@ -22,4 +22,23 @@ func findBottomLeftValue(root *TreeNode) int {
 		}
 	}
 	return root.Val
+}
+
+// https://leetcode.com/problems/find-bottom-left-tree-value/discuss/98802/Simple-Java-Solution-beats-100.0!
+func findBottomLeftValue(root *TreeNode) int {
+	return doFindBottomLeftValue(root, 1, []int{0, 0})
+}
+
+func doFindBottomLeftValue(root *TreeNode, depth int, res []int) int {
+	if res[1] < depth {
+		res[0] = root.Val
+		res[1] = depth // res[1] represents maxDepth
+	}
+	if root.Left != nil {
+		doFindBottomLeftValue(root.Left, depth+1, res)
+	}
+	if root.Right != nil {
+		doFindBottomLeftValue(root.Right, depth+1, res)
+	}
+	return res[0]
 }
