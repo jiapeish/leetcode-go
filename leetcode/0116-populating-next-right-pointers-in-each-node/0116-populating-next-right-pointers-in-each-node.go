0bfbd35db191ccc1440d5be6f2f325cae85b0875
jiapeish <jiapeish@gmail.com>
2021-06-08 23:26:44 +0800

    tree: 0116

--- leetcode/0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.go
+++ leetcode/0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.go
@@ -8,5 +8,45 @@ type Node struct {
 }
 
 func connect(root *Node) *Node {
+	dfs(root, nil)
+	return root;
+}
 
+func dfs(curr *Node, next *Node) {
+	if curr == nil {
+		return
+	}
+
+	curr.Next = next;
+	dfs(curr.Left, curr.Right)
+
+	if curr.Next == nil {
+		dfs(curr.Right, nil)
+	} else {
+		dfs(curr.Right, curr.Next.Left)
+	}
+}
+
+// todo wrong!
+func connectIter(root *Node) *Node {
+	if root == nil {
+		return root
+	}
+
+	var pre = root
+	var cur *Node
+
+	for pre.Left != nil {
+		cur = pre
+		for cur != nil {
+			cur.Left.Next = cur.Right
+			if cur.Next != nil {
+				cur.Right.Next = cur.Next.Left
+			}
+			cur = cur.Next
+		}
+		pre = pre.Next
+	}
+
+	return root
 }
---
+++ leetcode/0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.go
@@ -0,0 +1,12 @@
+package main
+
+type Node struct {
+	Val int
+	Left *Node
+	Right *Node
+	Next *Node
+}
+
+func connect(root *Node) *Node {
+
+}
